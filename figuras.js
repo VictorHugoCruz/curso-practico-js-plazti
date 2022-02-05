e6ec89e347baa4137efa76f167f3523f8b1852db
victorhcc <huguitobody26@gmail.com>
2022-02-05 18:42:31 -0400

    añadimos funciones para el triangulo y para el area

--- figuras.js
+++ figuras.js
@@ -19,3 +19,53 @@ function calcularAreaCuadrado(){
 
     alert(areaCuadrado(value));
 }
+
+//Este bloque empieza la seccion de triangulo
+function perimetroTriangulo(base){
+    
+    return base*3;
+}
+function areaTriangulo(base, altura){
+    return (base*altura)/2;
+}
+
+function calcularAreaTriangulo(){
+    const base = document.getElementById("InputTriangulo");
+    const value = base.value;
+
+    const altura = document.getElementById("InputAltura");
+    const valor = altura.value;
+
+    alert(areaTriangulo(value, valor));
+}
+
+function calcularPerimetroTriangulo(){
+    const base = document.getElementById("InputTriangulo");
+    const value = base.value;
+
+    alert(perimetroTriangulo(value));
+}
+
+//funciones para la circunferencia
+
+function perimetroCirculo(radio){
+    return 2*Math.PI*radio;
+}
+
+function areaCirculo(radio){
+    return Math.PI*radio*radio;
+}
+
+function calcularPerimetroCirculo(){
+    const input = document.getElementById("InputCirculo");
+    const value = input.value;
+
+    alert(perimetroCirculo(value));
+}
+
+function calcularAreaCirculo(){
+    const input = document.getElementById("InputCirculo");
+    const value = input.value;
+
+    alert(areaCirculo(value));
+}
